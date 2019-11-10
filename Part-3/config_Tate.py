e9581104274a7d0249e5e8aa7dce7e08509d89ba
Woman <52986268+tateha26@users.noreply.github.com>
2019-11-10 00:07:18 -0500

    Homework

---
+++ Part-3/config_Tate.py
@@ -0,0 +1,27 @@
+
+#With the API Network, your site users can tap into real estate data and information provided by Zillow. 
+#You have signed up for the following APIs: Directory API, Postings API, Profile Summary API, 
+#Property Details API, Reviews API, Valuation API
+#Your Zillow Web Services Identification (ZWSID) is: 
+zillow_api = "X1-ZWz1hfy7k0pnuz_a80zd"
+
+#U.S. Department of Labor API Key
+dol_key = "9a58d0d6-8453-4473-bcf7-11e9b3452a83"
+
+#WiGLE API Key
+
+
+#Twitter API Keys/Logins
+consumer_key = "rY13skw3CeFM1biD58DGN6FN9"
+consumer_secret = "BVnHIVOx6doDvqJ9NLM2fFSjOvtNTyE9z76VmeG4EZ8BI2cOke"
+access_token = "1183870185918418949-RyIvfMfrqAX4vVNpS9YrZa8XG4NAUd"
+access_token_secret = "JE3Kgq5CkGXIaAm8RJI0XQhQHr5UVeWhv7WEh1aAjzreL"
+
+#Google API Keys.
+google_key = "AIzaSyDnSoq2mfyxOlSDxJIdO2CMWGyQi4nrRWk"
+
+#MapBox API Key.
+mapbox_key = "sk.eyJ1IjoidGF0ZS1kZXZlbG9wZXIiLCJhIjoiY2syaHpubDU4MHIwdDNtcGFkY2U2cGE1aCJ9.vqCbXVYd1WwrixaNZSpZig"
+
+#Open Weather API Keys.
+weather_api = "3d37a48bc100d44c1e11044fb5fb2e6a"
